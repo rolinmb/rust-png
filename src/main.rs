0f4ba1228b7bb13ee68ec91d0d42d3f0c5e2dbb8
Rolin Blake <rolinmblake@gmail.com>
2023-12-20 07:31:22 -0600

    added custom png generation with rgb channel functions, also some cleaning up

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ const INVALIDCHARS: &[&str; 16] = &[
 fn pngcopy(pngname: &str, outname: &str) {
   let srcpng = image::open(Path::new(&pngname)).unwrap();
   println!("\npngcopy(): Successfully loaded {}", pngname);
-  println!("  -> {} Dimensions: {:?}", pngname, (srcpng.width(), srcpng.height()));
+  println!("  -> {} Dimensions: {:?}", pngname, srcpng.dimensions());
   println!("  -> {} Pixel Color Type: {:?}", pngname, srcpng.color());
   let srcpng = srcpng.to_rgba8();
   let mut newpng: RgbaImage = ImageBuffer::new(srcpng.width(), srcpng.height());
@@ -40,7 +40,7 @@ fn rgbainvert(clr: &Rgba<u8>) -> Rgba<u8> {
 fn pnginvert(pngname: &str, outname: &str) {
   let srcpng = image::open(Path::new(&pngname)).unwrap();
   println!("\npnginvert(): Successfully loaded {}", pngname);
-  println!("  -> {} Dimensions: {:?}", pngname,  (srcpng.width(), srcpng.height()));
+  println!("  -> {} Dimensions: {:?}", pngname,  srcpng.dimensions());
   println!("  -> {} Pixel Color Type: {:?}", pngname, srcpng.color());
   let srcpng = srcpng.to_rgba8();
   let mut newpng: RgbaImage = ImageBuffer::new(srcpng.width(), srcpng.height());
@@ -57,23 +57,42 @@ fn pnginvert(pngname: &str, outname: &str) {
 fn pngedges(pngname: &str, outname: &str) {
   let srcpng = image::open(&pngname).unwrap();
   println!("\nedgedetect(): Successfully loaded {}", pngname);
-  println!("  -> {} Dimensions: {:?}", pngname, (srcpng.width(), srcpng.height()));
+  println!("  -> {} Dimensions: {:?}", pngname, srcpng.dimensions());
   println!("  -> {} Pixel Color Type: {:?}", pngname, srcpng.color());
   let graypng = srcpng.to_luma8();
   let gradx = imageops::filter3x3(&graypng, &SOBELHORIZ);
   let grady = imageops::filter3x3(&graypng, &SOBELVERTI);
-  let mut edgemag = RgbaImage::new(srcpng.width(), srcpng.height());
+  let mut edgepng = RgbaImage::new(srcpng.width(), srcpng.height());
   for x in 0..srcpng.width() {
     for y in 0..srcpng.height() {
       let magx = gradx.get_pixel(x, y)[0] as f32;
       let magy = grady.get_pixel(x, y)[0] as f32;
       let mag = (magx.powi(2) + magy.powi(2)).sqrt() as u8;
       let pxl = Rgba([mag, mag, mag, 255]);
-      edgemag.put_pixel(x, y, pxl);
+      edgepng.put_pixel(x, y, pxl);
     }
   }
-  edgemag.save(outname).unwrap();
-  println!("\nedgedetect(): Scucessfully saved / rewrote {}", outname);
+  edgepng.save(outname).unwrap();
+  println!("\nedgedetect(): Successfully saved / rewrote {}", outname);
+}
+
+fn custompng(
+  outname: &str,
+  width: u32,
+  height: u32,
+  f_r: impl Fn(u32, u32) -> u32,
+  f_g: impl Fn(u32, u32) -> u32,
+  f_b: impl Fn(u32, u32) -> u32,
+) { 
+  let mut newpng = RgbaImage::new(width, height);
+  for x in 0..newpng.width() {
+    for y in 0..newpng.height() {
+      let pxl = Rgba([f_r(x, y) as u8, f_g(x, y) as u8, f_b(x, y) as u8, 255]);
+      newpng.put_pixel(x, y, pxl);
+    }
+  }
+  newpng.save(outname).unwrap();
+  println!("\ncustompng(): Successfully saved / rewrote {}", outname);
 }
 
 fn main() {
@@ -97,11 +116,18 @@ fn main() {
   } else {
     panic!("main(): Please enter a .png name to output to src/png_out as the second argument");
   };
-  let pngname = format!("src/png_in/{}.png", &pngname);
+  //let pngname = format!("src/png_in/{}.png", &pngname);
   //let copyname = format!("src/png_out/{}.png", &outname);
-  let invname = format!("src/png_out/{}_i.png", &outname);
-  let edgname = format!("src/png_out/{}_e.png", &outname);
+  //let invname = format!("src/png_out/{}_i.png", &outname);
+  //let edgname = format!("src/png_out/{}_e.png", &outname);
+  let customname = format!("src/png_out/{}_c.png", &outname);
   //pngcopy(&pngname, &copyname);
-  pnginvert(&pngname, &invname);
-  pngedges(&pngname, &edgname);
+  //pnginvert(&pngname, &invname);
+  //pngedges(&pngname, &edgname);
+  custompng(
+    &customname, 1000, 1000,
+    |x, y| (x*x + y) as u32,
+    |x, y| (x + y).wrapping_mul((x.wrapping_sub(y))) as u32,
+    |x, y| (x * y) as u32,
+  );
 }
